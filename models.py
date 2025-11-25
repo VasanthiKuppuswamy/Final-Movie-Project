43cb56e997943b2b4fbc4afb4615ffd0f8484743
VasanthiKuppuswamy <Vasanthik@gmx.net>
2025-11-25 16:54:44 +0100

    added user and movie model

--- models.py
+++ models.py
@@ -1,2 +1,35 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    movies = db.relationship('Movie', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f"User(id={self.id}, name='{self.name}')"
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    director = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer)
+    poster_url = db.Column(db.String(200))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Movie(id={self.id}, name='{self.name}', year='{self.year}')"
+
+
+
+
+
+
+
+
+
+
+
+
